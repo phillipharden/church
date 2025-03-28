e2c741b2f6d1f86f1eba8a1506c064ecef205230
PhillipHarden <phillipiscoding@gmail.com>
2025-03-28 19:19:36 -0400

    header & footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome To Church</h1>
-    </div>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Church</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
